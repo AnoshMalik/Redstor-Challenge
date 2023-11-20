d43dd1c1b51d19055ce863fde67c8efd771add48
Anosh Malik <anoshmalik.am@gmail.com>
2023-11-20 16:24:02 +0000

    Added button counter for A

--- redstor-challenge/src/App.js
+++ redstor-challenge/src/App.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+
+
+export default function App() {
+
+const [aCounter, setACounter] = useState(0);
+
+const incrementA = () => {
+  setACounter(aCounter + 1);
+  console.log(`A's current value = ${aCounter}`);
+};
+  
   return (
-    <div className="App">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+        <img src="" alt='A'></img>
+      <p>Cost</p>
+      <button onClick={incrementA}>Add A</button>
     </div>
   );
-}
+};
 
-export default App;
+
+
+
