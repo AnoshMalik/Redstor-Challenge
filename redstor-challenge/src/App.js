7022442fc4cb6c3cea821a9ad88243eb23084ef1
Anosh Malik <anoshmalik.am@gmail.com>
2023-11-20 16:39:45 +0000

    Added 4 buttons for each item

--- redstor-challenge/src/App.js
+++ redstor-challenge/src/App.js
@@ -1,26 +1,67 @@
-import { useState } from 'react';
-import './App.css';
-
-
+import { useState } from "react";
+import "./App.css";
 
 export default function App() {
+  const [aCounter, setACounter] = useState(0);
+  const [aCost, setACost] = useState(60);
+  const [bCounter, setBCounter] = useState(0);
+  const [bCost, setBCost] = useState(30);
+  const [cCounter, setCCounter] = useState(0);
+  const [cCost, setCCost] = useState(30);
+  const [dCounter, setdCounter] = useState(0);
+  const [dCost, setDCost] = useState(25);
 
-const [aCounter, setACounter] = useState(0);
+  const incrementA = () => {
+    setACounter(aCounter + 1);
+    console.log(`A's current value = ${aCounter}`);
+  };
+  const incrementB = () => {
+    setACounter(aCounter + 1);
+    console.log(`A's current value = ${aCounter}`);
+  };
+  const incrementC = () => {
+    setACounter(aCounter + 1);
+    console.log(`A's current value = ${aCounter}`);
+  };
+  const incrementD = () => {
+    setACounter(aCounter + 1);
+    console.log(`A's current value = ${aCounter}`);
+  };
 
-const incrementA = () => {
-  setACounter(aCounter + 1);
-  console.log(`A's current value = ${aCounter}`);
-};
-  
   return (
     <div>
-        <img src="" alt='A'></img>
-      <p>Cost</p>
-      <button onClick={incrementA}>Add A</button>
+      <div>
+        <h1>REDSTOR CHALLENGE</h1>
+      </div>
+      <div>
+        <span>
+          <div>
+            <img src="" alt="A"></img>
+            <p>Cost</p>
+            <button onClick={incrementA}>Add A</button>
+          </div>
+          <div>
+            <img src="" alt="B"></img>
+            <p>Cost</p>
+            <button onClick={incrementB}>Add B</button>
+          </div>
+          <div>
+            <img src="" alt="C"></img>
+            <p>Cost</p>
+            <button onClick={incrementC}>Add C</button>
+          </div>
+          <div>
+            <img src="" alt="D"></img>
+            <p>Cost</p>
+            <button onClick={incrementD}>Add D</button>
+          </div>
+        </span>
+      </div>
+      <div>
+        <span>
+          <h3>CART</h3>
+        </span>
+      </div>
     </div>
   );
-};
-
-
-
-
+}
